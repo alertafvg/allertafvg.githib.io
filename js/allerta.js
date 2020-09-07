3e8718e2849f47fda6d489d919f964a3ab6efe42
binc0 <42704408+binc0@users.noreply.github.com>
2020-09-07 19:09:22 +0200

    Update allerta.js

--- js/allerta.js
+++ js/allerta.js
@@ -9,7 +9,7 @@ var settings = {
             if (data[0].level >= 1) {
             allerta.innerHTML += '<div class="alert alert-danger" role="alert"> <h4 class="alert-heading">' + data[0].title + '</h4> <p>' + data[0].description + '</p>  <hr>  <a class="btn btn-primary" href="' + data[0].link_url + '" target="_blank" role="button">Allerta</a> <a class="btn btn-secondary" href="' + data[0].file_url + '" target="_blank" role="button">Bollettino PDF</a></div>';
             } else {
-             allerta.innerHTML += '    <div class="alert alert-success" role="alert"><h4 class="alert-heading">NESSUNA ALLERTA IN COSRO</h4><p>Attiva le notifiche per restare aggiornato.</p><hr> <a class="btn btn-primary text-white" role="button" href="#notifiche">Attiva le notifiche</a></div>';   
+             allerta.innerHTML += '    <div class="alert alert-success" role="alert"><h4 class="alert-heading">NESSUNA ALLERTA IN CORSO</h4><p>Attiva le notifiche per restare aggiornato.</p><hr> <a class="btn btn-primary text-white" role="button" href="#notifiche">Attiva le notifiche</a></div>';   
             }
 
 });
