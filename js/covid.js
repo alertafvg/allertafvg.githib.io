2e5c861b07768ddc359eafcf4e5f38d581f9f136
binc0 <42704408+binc0@users.noreply.github.com>
2020-12-21 21:23:36 +0100

    rm table

--- js/covid.js
+++ js/covid.js
@@ -10,7 +10,7 @@ function dateToNiceString(myDate){
 
 
 
-var table = document.getElementById("tabella");
+// var table = document.getElementById("tabella");
 
 //var map = document.getElementById("map");
 
@@ -33,7 +33,7 @@ Papa.parse("/data.csv", {
     header: true,
     fastMode: true,
 	step: function(row) {
-        table.innerHTML += '<tr> <th scope="row">'+ row.data.ISTAT +'</th>     <td>'+ row.data.Comune+'</td>     <td>'+ row.data.Popolazione +'</td>      <td>'+ row.data.Positivi +'</td>        <td>'+ row.data.Quarantena +'</td>       <td>'+ row.data.Guariti +'</td><td>'+ row.data.Morti +'</td></tr>';
+        // table.innerHTML += '<tr> <th scope="row">'+ row.data.ISTAT +'</th>     <td>'+ row.data.Comune+'</td>     <td>'+ row.data.Popolazione +'</td>      <td>'+ row.data.Positivi +'</td>        <td>'+ row.data.Quarantena +'</td>       <td>'+ row.data.Guariti +'</td><td>'+ row.data.Morti +'</td></tr>';
         quarantena.push(row.data.Quarantena);
         positivi.push(row.data.Positivi);
         guariti.push(row.data.Guariti);
